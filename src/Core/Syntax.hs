0ada914d823b69b9a589034694e956d8277aecdd
jam-khan <jamkhan@connect.hku.hk>
2025-01-05 15:34:16 +0800

    syntax for core

--- src/Core/Syntax.hs
+++ src/Core/Syntax.hs
@@ -40,19 +40,20 @@ module Core.Syntax where
         -- Type Systems  : To be done
 
 
-data Exp =  Ctx                     -- Context
-        |   Unit                    -- Unit
-        |   Lit    Integer          -- Integer literal
-        |   EBool  Bool             -- Boolean Term
-        |   EString String          -- String Term
-        |   BinOp  BinaryOp Exp Exp -- Binary operations: Application, Box and Merge
-        |   Lam    Typ Exp          -- Lambda Abstraction
-        |   Proj   Exp Int          -- Projection
-        |   Clos   Exp Exp          -- Closure
-        |   Rec    String Exp       -- Single-Field Record
-        |   RProj  Exp String       -- Record Projection by Label
+data Exp =  Ctx                      -- Context
+        |   Unit                     -- Unit
+        |   Lit    Integer           -- Integer literal
+        |   Lam    Typ Exp           -- Lambda Abstraction
+        |   Proj   Exp Int           -- Projection
+        |   Clos   Exp Exp           -- Closure
+        |   Rec    String Exp        -- Single-Field Record
+        |   RProj  Exp String        -- Record Projection by Label
+        |   App    Exp Exp           -- Application
+        |   Mrg    Exp Exp           -- Merge
+        |   Box    Exp Exp           -- Box
         -- Extensions
-        |   UnOp   UnaryOp Exp       -- Unary operations:  Not
+        |   EBool   Bool             -- Boolean Term
+        |   EString String           -- String Term
         |   If     Exp Exp Exp       -- Conditionals
         |   Let    Exp Exp           -- Let Bindings
         |   Fix    Exp               -- Recursion
@@ -62,13 +63,16 @@ data Exp =  Ctx                     -- Context
         |   Fst    Exp               -- First Projection
         |   Snd    Exp               -- Second Projection
         -- Sums
-        |   InL    Typ Exp           -- 
-        |   InR    Typ Exp
-        |   Case   Exp String Exp String Exp
+        |   InL    Typ Exp           -- Tagging Left
+        |   InR    Typ Exp           -- Tagging Right
+        |   Case   Exp Exp Exp       -- Case of Sums
         -- Built-in Lists
-        |   Nil    Typ               -- Nil for list
-        |   Cons   Exp Exp           -- List
-        |   LCase  Exp Exp String String Exp 
+        |   Nil    Typ               -- Nil List
+        |   Cons   Exp Exp           -- Cons for List
+        |   LCase  Exp Exp Exp       -- Case of List
+        -- Operations
+        |   BinOp  BinaryOp Exp Exp  -- Binary operations
+        |   UnOp   UnaryOp Exp       -- Unary operations
         deriving Eq
 
 
@@ -83,7 +87,6 @@ data Value =    VUnit                   -- Unit value
         |       VRcd String Value       -- Single-field record value
         |       VMrg Value Value        -- Merge of two values
         -- Extensions
-        |       VFix Exp
         |       VBool Bool              -- Boolean Value
         |       VString String          -- String Value
         -- Pair extension
@@ -106,23 +109,17 @@ data Typ =  TUnit                  -- Unit type for empty environment
         -- Extensions
         |   TBool                  -- Boolean type
         |   TString                -- String type
-        -- Type for List
         |   TList  Typ             -- Type for built-in list
-        -- Type for sums
         |   TSum   Typ Typ         -- Type for sums
+        |   TPair  Typ Typ      
         deriving Eq
 
 
 data UnaryOp    =       Not
-                |       Index Int
         deriving Eq
 
 -- Operations Definitions
-data BinaryOp   =       App             -- Application
-                |       Box             -- Box
-                |       Mrg             -- Merge
-                --      Extensions
-                |       Arith ArithOp   -- Arithmetic
+data BinaryOp  =        Arith ArithOp   -- Arithmetic
                 |       Comp  CompOp    -- CompOp
                 |       Logic LogicOp   -- Boolean Logic
 
@@ -138,10 +135,7 @@ instance Show BinaryOp where
         show (Arith op) = show op
         show (Comp op)  = show op
         show (Logic op) = show op
-        show App        = "App"
-        show Box        = "\x25B8"
-        show Mrg        = " ,, "
-
+        
 instance Show ArithOp where
         show :: ArithOp -> String
         show Add = "+"
@@ -173,9 +167,6 @@ instance Eq BinaryOp where
         (Arith op1) == (Arith op2) = op1 == op2
         (Comp op1)  == (Comp op2)  = op1 == op2
         (Logic op1) == (Logic op2) = op1 == op2
-        App         == App         = True
-        Box         == Box         = True
-        Mrg         == Mrg         = True
         _           == _           = False
 
 
@@ -195,34 +186,37 @@ instance Show Exp where
 showIndented :: Int -> Exp -> String
 showIndented n exp = indent 0 ++ show' n exp
         where
-                indent :: Int -> String
-                indent n1 = replicate n1 ' '
+        indent :: Int -> String
+        indent n1 = replicate n1 ' '
 
-                show' :: Int -> Exp -> String
-                show' _ Ctx                = "?"
-                show' _ Unit               = "\x03B5"
-                show' _ (Lit i)            = show i
-                show' n (BinOp op e1 e2)   = "(" ++ show' n e1 ++ " " ++ show op ++ " " ++ show' n e2 ++ ")"
-                show' n (UnOp op e)        = show op ++ "(" ++ show' n e ++ ")"
-                show' n (Lam typ e)        =  "\n"
-                                                ++ indent n ++ "\x03BB" ++ " " ++ show typ ++ " . \n" 
-                                                        ++ indent (n + 2) ++ show' (n + 2) e
-                show' n (Proj e n')        = show' n e ++ "." ++ show n'
-                show' n (Clos e1 e2)       = "< " ++ show' n e1 ++ ", " ++ show' (n + 2) e2 ++ " >"
-                show' n (Rec s e)          = "{ " ++ show s  ++ " = " ++ show' (n + 2) e ++ " }"
-                show' n (RProj e s)        = show' n e ++ "." ++ show s
-                show' n (If c e1 e2)       = "IF " ++ show' (n + 2) c ++ "\n" 
-                                                ++ indent (n + 4) ++ "THEN \n" ++ indent (n + 6) ++ show' (n + 4) e1 ++ "\n" 
-                                                ++ indent (n + 4) ++ "ELSE \n" ++ indent (n + 6) ++ show' (n + 4) e2 ++ "\n"
-                show' n (Let e1 e2)        =    "LET \n" 
-                                                        ++ indent n ++ show' n e1 ++
-                                                "\n IN " 
-                                                        ++ indent n ++ show' (n + 2) e2
-                show' _ (EBool b)          = show b
-                show' n (Fix e)            = indent n ++ "fix " ++ show' (n + 2) e
-                show' n (Pair e1 e2)       = "(" ++ show' n e1 ++ ", " ++ show' n e2 ++ ")"
-                show' _ (Nil typ)          = indent n ++ "NIL of " ++ show typ
-                show' n (Cons e1 e2)       = indent n ++ show' n e1 ++ " :: " ++ show' n e2
+        show' :: Int -> Exp -> String
+        show' _ Ctx                = "?"
+        show' _ Unit               = "\x03B5"
+        show' _ (Lit i)            = show i
+        show' n (BinOp op e1 e2)   = "(" ++ show' n e1 ++ " " ++ show op ++ " " ++ show' n e2 ++ ")"
+        show' n (UnOp op e)        = show op ++ "(" ++ show' n e ++ ")"
+        show' n (Lam typ e)        =  "\n"
+                                        ++ indent n ++ "\x03BB" ++ " " ++ show typ ++ " . \n" 
+                                                ++ indent (n + 2) ++ show' (n + 2) e
+        show' n (Proj e n')        = show' n e ++ "." ++ show n'
+        show' n (Clos e1 e2)       = "< " ++ show' n e1 ++ ", " ++ show' (n + 2) e2 ++ " >"
+        show' n (Rec s e)          = "{ " ++ show s  ++ " = " ++ show' (n + 2) e ++ " }"
+        show' n (RProj e s)        = show' n e ++ "." ++ show s
+        show' n (If c e1 e2)       = "IF " ++ show' (n + 2) c ++ "\n" 
+                                        ++ indent (n + 4) ++ "THEN \n" ++ indent (n + 6) ++ show' (n + 4) e1 ++ "\n" 
+                                        ++ indent (n + 4) ++ "ELSE \n" ++ indent (n + 6) ++ show' (n + 4) e2 ++ "\n"
+        show' n (Let e1 e2)        =    "LET \n" 
+                                                ++ indent n ++ show' n e1 ++
+                                        "\n IN " 
+                                                ++ indent n ++ show' (n + 2) e2
+        show' _ (EBool b)          = show b
+        show' n (Fix e)            = indent n ++ "fix " ++ show' (n + 2) e
+        show' n (Pair e1 e2)       = "(" ++ show' n e1 ++ ", " ++ show' n e2 ++ ")"
+        show' _ (Nil typ)          = indent n ++ "NIL of " ++ show typ
+        show' n (Cons e1 e2)       = indent n ++ show' n e1 ++ " :: " ++ show' n e2
+        show' n (App e1 e2)        = "(" ++ show' n e1 ++ ")" ++ "(" ++ show' n e2 ++ ")" 
+        show' n (Box e1 e2)        = "(" ++ show' n e1 ++ "\x25B8" ++ show' n e2 ++ ")"
+        show' n (Mrg e1 e2)        = "(" ++ show' n e2 ++ ")" ++ " ,, " ++ "(" ++ show' n e2 ++ ")"
 
 
 instance Show Typ where 
@@ -237,11 +231,11 @@ instance Show Typ where
         
 instance Show Value where
         show :: Value -> String
-        show VUnit                      = "\x03B5"
-        show (VInt i)                   = show i
-        show (VBool b)                  = show b
-        show (VClos val exp)        =   "< { " ++ show val ++ " }, \n(" ++ showIndented 6 exp ++ "}"
-        show (VRcd label val)           = "{ " ++ show label ++ " = "  ++ show val ++ " }"             
-        show (VMrg v1 v2)               = show v1 ++ " ,, " ++ show v2
-        show (VNil typ)                 = "nil of " ++ show typ
-        show (VCons head rest)          = show head ++ " :: " ++ show rest
+        show VUnit               = "\x03B5"
+        show (VInt i)            = show i
+        show (VBool b)           = show b
+        show (VClos val exp)     =   "< { " ++ show val ++ " }, \n(" ++ showIndented 6 exp ++ "}"
+        show (VRcd label val)    = "{ " ++ show label ++ " = "  ++ show val ++ " }"             
+        show (VMrg v1 v2)        = show v1 ++ " ,, " ++ show v2
+        show (VNil typ)          = "nil of " ++ show typ
+        show (VCons head rest)   = show head ++ " :: " ++ show rest
