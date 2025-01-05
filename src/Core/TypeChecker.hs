59517ee7e335d2cdb746ca8aa4243461c906b26d
jam-khan <jamkhan@connect.hku.hk>
2025-01-06 03:40:43 +0800

    Core calculus fixed

--- src/Core/TypeChecker.hs
+++ src/Core/TypeChecker.hs
@@ -39,131 +39,124 @@ rlookupt _ _                = Nothing
 -- ==> direction
 infer :: Typ -> Exp -> Maybe Typ
 -- TYP-CTX
-infer ctx Ctx               = Just ctx
+infer ctx Ctx                   = Just ctx
 -- TYP-PROJ
-infer ctx (Proj e n)        = lookupt tB n
-                            where Just tB = infer ctx e
+infer ctx (Proj e n)            = lookupt tB n
+                                    where Just tB = infer ctx e
 -- TYP-LIT
-infer ctx (Lit i)           = Just TInt
+infer ctx (Lit i)               = Just TInt
 -- TYP-TOP
-infer ctx Unit              = Just TUnit
+infer ctx Unit                  = Just TUnit
 -- TYP-BOX
-infer ctx (Box e1 e2) = infer ctx1 e2
-                            where Just ctx1 = infer ctx e1
+infer ctx (Box e1 e2)           = infer ctx1 e2
+                                    where Just ctx1 = infer ctx e1
 -- TYP-MERGE
-infer ctx (Mrg e1 e2) = Just (TAnd tA tB)
-                            where   Just tA = infer ctx e1
-                                    Just tB = infer (TAnd ctx tA) e2
+infer ctx (Mrg e1 e2)           = Just (TAnd tA tB)
+                                    where   Just tA = infer ctx e1
+                                            Just tB = infer (TAnd ctx tA) e2
 -- TYP-APP
-infer ctx (App e1 e2) = 
-    case infer ctx e1 of
-        Just (TArrow tA tB) ->  if  check ctx e2 tA 
-                                then Just tB
-                                else Nothing
-        _                   -> Nothing
+infer ctx (App e1 e2)           = case infer ctx e1 of
+                                        Just (TArrow tA tB) ->  if  check ctx e2 tA 
+                                                                    then Just tB
+                                                                    else Nothing
+                                        _                   -> Nothing
 -- TYP-LAM
-infer ctx (Lam tA e) = 
-    case infer (TAnd ctx tA) e of
-        Just tB -> Just (TArrow tA tB)
-        _       -> Nothing
-
+infer ctx (Lam tA e)            = case infer (TAnd ctx tA) e of
+                                        Just tB -> Just (TArrow tA tB)
+                                        _       -> Nothing
 -- TYP-CLOS
-infer ctx (Clos e1 (Lam tA e2)) =
-    case infer ctx e1 of
-        Just ctx1   -> 
-            case infer (TAnd ctx1 tA) e2 of
-                Just tB     -> Just (TArrow tA tB)
-                _           -> Nothing
-        _           -> Nothing
+infer ctx (Clos e1 (Lam tA e2)) = case infer ctx e1 of
+                                        Just ctx1   -> 
+                                            case infer (TAnd ctx1 tA) e2 of
+                                                Just tB     -> Just (TArrow tA tB)
+                                                _           -> Nothing
+                                        _           -> Nothing
 -- TYP-RCD
-infer ctx (Rec l e)     = Just (TRecord l tA)
-                        where Just tA = infer ctx e
+infer ctx (Rec l e)             = Just (TRecord l tA)
+                                    where Just tA = infer ctx e
 -- TYP-SEL
-infer ctx (RProj e l)   =
-    case infer ctx e of
-        Just tB     -> case rlookupt tB l of
-                        Just tA     -> if containment (TRecord l tA) tB
-                                            then Just tA else Nothing
-                        Nothing     -> Nothing
-        Nothing     -> Nothing
+infer ctx (RProj e l)           = case infer ctx e of
+                                        Just tB     -> case rlookupt tB l of
+                                                            Just tA     -> if containment (TRecord l tA) tB
+                                                                                then Just tA else Nothing
+                                                            Nothing     -> Nothing
+                                        _           -> Nothing
 
--- Extensions
 
-{-
-                    ----------------------------- (TYP-Bool)
-                        Gamma |- Bool => TBool
-
-        Gamma |- e1 <= Bool    Gamma |- e2 => t      Gamma |- e3 <= t
-        ------------------------------------------------------------- (TYP-IF)
-                        Gamma |- If e1 e2 e3 => t1
-        
-        Gamma |- e1 <= Bool    Gamma |- e2 => t      Gamma |- e3 <= t
-        ------------------------------------------------------------- (TYP-IF)
-                        Gamma |- e1 () => t1
-        
-
-         |- e1 <= Bool         v |- e2 => t           v |- e3 <= t
-        ----------------------------------------------------------- (TYP-IF)
-                        v |- If e1 e2 e3 => t1
--}
 -- TYP-BOOL
-infer ctx (EBool bool)      = Just TBool
+infer ctx (EBool _)             = Just TBool
+-- TYP-STRING
+infer ctx (EString _)           = Just TString
+-- TYP-FIX
+infer ctx (Fix (Lam tA e))      =   if      check (TAnd ctx tA) e (TArrow tA tA)
+                                    then    Just tA
+                                    else    Nothing
+-- TYP-LET
+infer ctx (Let e1 e2)           = case infer ctx e1 of
+                                        Just tA     -> infer (TAnd ctx tA) e2
+                                        _           -> Nothing
 -- TYP-IF
-infer ctx (If cond e1 e2)   =   if check ctx cond TBool && (t1 == t2)
+infer ctx (If cond e1 e2)       = if check ctx cond TBool && (t1 == t2)
                                     then Just t1
                                     else Nothing
                                 where
                                     Just t1 = infer ctx e1
                                     Just t2 = infer ctx e2
--- TYP-ARITH
-infer ctx (BinOp (Arith _) e1 e2) =
-    case infer ctx e1 of
-        Just TInt   ->  if      check ctx e2 TInt
-                        then    Just TInt
-                        else    Nothing
-        _           ->  Nothing
--- TYP-COMP
-infer ctx (BinOp (Comp _) e1 e2)    =
-    case infer ctx e1 of
-        Just TInt       ->  if      check ctx e2 TInt
-                            then    Just TBool
-                            else    Nothing
-        Just TBool      ->  if      check ctx e2 TBool
-                            then    Just TBool
-                            else    Nothing
-        _               ->  Nothing
--- TYP-LOGIC
-infer ctx (BinOp (Logic _) e1 e2)    =
-    case infer ctx e1 of
-        Just TBool      ->  if      check ctx e2 TBool
-                            then    Just TBool
-                            else    Nothing
-        _               ->  Nothing
--- TYP-LET
-infer ctx (Let e1 e2)   = 
-    case infer ctx e1 of
-        Just tA     -> infer (TAnd ctx tA) e2
-        _           -> Nothing
--- TYP-FIX
-infer ctx (Fix (Lam tA e))       =  if      check (TAnd ctx tA) e tA
-                                    then    Just tA
-                                    else    Nothing
-{--
-        Gamma |- t1 <= A        Gamma |- t2 => TList A
-        -------------------------------------------- TYP-Cons
-                Gamma |- cons t1 t2 => List A
-
-        -------------------------------------------- TYP-Nil
-                Gamma |- Nil A  => A
---}
--- TYP-CONS
-infer ctx (Cons t1 t2)      = case infer ctx t2 of
-                                    Just (TList tA)     ->  if check (TAnd ctx tA) t1 tA
-                                                        then Just (TList tA)
-                                                        else Nothing
-                                    _                   -> Nothing
+-- TYP-PAIR
+infer ctx (Pair e1 e2)          = TPair <$> infer ctx e1 <*> infer ctx e2
+-- TYP-FST
+infer ctx (Fst e)               = case infer ctx e of
+                                        Just (TPair tA tB) -> Just tA
+                                        _                  -> Nothing
+-- TYP-SND
+infer ctx (Snd e)               = case infer ctx e of
+                                        Just (TPair tA tB) -> Just tB
+                                        _                  -> Nothing
+-- TYP-INL
+infer ctx (InL tB e1)           = TSum <$> infer ctx e1 <*> Just tB
+-- TYP-INR
+infer ctx (InR tA e2)           = TSum <$> Just tA <*> infer ctx e2
+-- TYP-CASE
+infer ctx (Case e1 e2 e3)       = case infer ctx e1 of
+                                        Just (TSum t1 t2)   ->  
+                                                case infer (TAnd ctx t1) e2 of
+                                                    Just t3     -> if    check (TAnd ctx t2) e3 t3
+                                                                    then Just t3 else Nothing
+                                                    _           -> Nothing
+                                        _                   -> Nothing
 -- TYP-NIL
-infer ctx (Nil tA)          = Just tA
+infer ctx (Nil tA)              = Just tA
+-- TYP-CONS
+infer ctx (Cons t1 t2)          = case infer ctx t2 of
+                                        Just (TList tA)     ->  if check (TAnd ctx tA) t1 tA
+                                                                    then Just (TList tA)
+                                                                    else Nothing
+                                        _                   -> Nothing
+
+-- TYP-ARITH
+infer ctx (BinOp (Arith _) e1 e2) 
+                                = case infer ctx e1 of
+                                        Just TInt   ->  if      check ctx e2 TInt
+                                                        then    Just TInt
+                                                        else    Nothing
+                                        _           ->  Nothing
+-- TYP-COMP
+infer ctx (BinOp (Comp _) e1 e2)    
+                                = case infer ctx e1 of
+                                        Just TInt       ->  if      check ctx e2 TInt
+                                                            then    Just TBool
+                                                            else    Nothing
+                                        Just TBool      ->  if      check ctx e2 TBool
+                                                            then    Just TBool
+                                                            else    Nothing
+                                        _               ->  Nothing
+-- TYP-LOGIC
+infer ctx (BinOp (Logic _) e1 e2)
+                                = case infer ctx e1 of
+                                        Just TBool      ->  if      check ctx e2 TBool
+                                                            then    Just TBool
+                                                            else    Nothing
+                                        _               ->  Nothing
 
 check :: Typ -> Exp -> Typ -> Bool
 check ctx (Nil t1) t2 = t1 == t2
