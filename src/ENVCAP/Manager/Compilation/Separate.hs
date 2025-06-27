5bf1ed45eda6fae5a1ec2cce0d71f725ac8b7423
jam-khan <jamkhan@connect.hku.hk>
2025-06-27 18:44:12 +0200

    AST simplified -- removing all the unnecessary features

--- src/ENVCAP/Manager/Compilation/Separate.hs
+++
@@ -1,1 +0,0 @@
-module ENVCAP.Manager.Compilation.Separate where
--- src/ENVCAP/Syntax.hs
+++ src/ENVCAP/Syntax.hs
@@ -8,125 +8,44 @@ module ENVCAP.Syntax where
 import GHC.Generics (Generic)
 import Data.Binary 
 
-type Params             = [(String, SurfaceTyp)]
-type Letargs            = [(String, SurfaceTyp, SurfaceTm)] 
+type Params             = [(String, SourceTyp)]
+type Letargs            = [(String, SourceTyp, SourceTm)] 
 type Name               = String
 type Pattern            = (String, [String])
-type Cases              = [(Pattern, SurfaceTm)]
+type Cases              = [(Pattern, SourceTm)]
 type Imports            = [String]
-
--- data Requirement        = Req String String | Param String SurfaceTyp
---                         deriving (Eq, Show)
--- For sake of simplicity, parameter requirements are not allowed yet.
-data Requirement        = Req String String
-                        deriving (Eq, Show)
-
-type Requirements       = [Requirement]
-type ParseImplData      = (Name, Authority, Imports, Requirements, SurfaceTm)
-type ParseIntfData      = (Name, Authority,          Requirements, Interface)
-
-type Interface          = [InterfaceStmt]
-
-data InterfaceStmt      =   IAliasTyp       String SurfaceTyp
-                        |   IAliasIntf      String Interface
-                        |   IType           SurfaceTyp
-                        |   IIden           String
-                        |   FunctionTyp     Name Params SurfaceTyp
-                        |   ModuleTyp       Name Params Interface
-                        |   Binding         Name SurfaceTyp
-                        deriving (Eq, Show)
-
-type Statements         = [SurfaceTm]
-
-data SurfaceTm          =   SCtx                                        -- Query
-                        |   SUnit                                       -- Unit
-                        |   SLit       Integer                          -- Integer Literal
-                        |   SBool      Bool                             -- Boolean Literal
-                        |   SString    String                           -- String  Literal
-                        |   SLam       Params SurfaceTm                 -- Abstraction with binding
-                        |   SClos      SurfaceTm Params SurfaceTm
-                        |   SRec       String SurfaceTm
-                        |   SRProj     SurfaceTm String
-                        |   SProj      SurfaceTm Integer                -- Projection on Expression
-                        |   SApp       SurfaceTm [SurfaceTm]
-                        |   SMrg       SurfaceTm SurfaceTm
-                        |   SBox       SurfaceTm SurfaceTm
-                        |   SVar       String
-                        |   SStruct    Params   SurfaceTm
-                        |   SFunc      Name Params SurfaceTyp SurfaceTm
-                        |   SModule    Name Params SurfaceTm
-                        {-- CAREFUL: Type Expansion --}
-                        |   SAliasTyp  String SurfaceTyp
-                        |   SAliasIntf String Interface
-                        {-- CAREFUL: Type Expansion --}
-                        |   SLet       Letargs  SurfaceTm
-                        |   SLetrec    Letargs  SurfaceTm
-                        |   SBinOp     BinaryOp SurfaceTm SurfaceTm
-                        |   SUnOp      UnaryOp  SurfaceTm
-                        |   SAnno      SurfaceTm SurfaceTyp
-                        |   SIf        SurfaceTm SurfaceTm SurfaceTm
-                        |   SPair      SurfaceTm SurfaceTm
-                        |   STuple     [SurfaceTm]
-                        |   SSwitch    SurfaceTm [(SurfaceTm, SurfaceTm)]
-                        |   SADTInst   (String, [SurfaceTm]) SurfaceTyp 
-                        |   SCase      SurfaceTm Cases
-                        |   SOpen      SurfaceTm SurfaceTm
-                        -- List matching
-                        |   SNil       SurfaceTyp
-                        |   SCons      SurfaceTm SurfaceTm
-                        |   SList      [SurfaceTm] SurfaceTyp
-                        deriving (Eq, Show)
-
-data SurfaceTyp         =   STUnit                              -- ^ Unit type for empty environment
-                        |   STInt                               -- ^ Integer type
-                        |   STBool                              -- ^ Boolean type
-                        |   STString                            -- ^ String type
-                        |   STAnd       SurfaceTyp SurfaceTyp   -- ^ Intersection type
-                        |   STArrow     SurfaceTyp SurfaceTyp   -- ^ Arrow type, e.g. A -> B
-                        |   STRecord    String     SurfaceTyp   -- ^ Single-Field Record Type
-                        |   STUnion     SurfaceTyp SurfaceTyp   -- ^ Union
-                        -- Extensions
-                        |   STList      SurfaceTyp              -- ^ Type for built-in list 
-                        |   STSig       SurfaceTyp SurfaceTyp   -- ^ Sig Type End
-                        |   STIden      String                  -- ^ Simply an alias
-                        deriving (Eq, Show)
-
-data SourceFragment     = TmFragment Name Authority SourceImport SourceRequirements SourceTyp
-data Authority          = Pure | Resource 
-                        deriving (Eq, Show)
-
-data SourceHeader       = TmInterface Name Authority SourceRequirements SourceTyp
 type SourceImport       = [(String, SourceHeader)]
 type SourceRequirements = [(String, SourceHeader)]
 
+
+data SourceFragment     = TmFragment Name Authority SourceImport SourceRequirements SourceIntf
+data Authority          = Pure | Resource 
+                        deriving (Eq, Show)
+
+data SourceHeader       = TmInterface Name Authority SourceRequirements SourceIntf
+
+type SourceIntf         = SourceTyp -- Placeholder for now
 data SourceTm           =   TmCtx                               -- Query
                         |   TmUnit                              -- Unit
                         |   TmLit       Integer                 -- Integer Literal
                         |   TmBool      Bool                    -- Boolean Literal
                         |   TmString    String                  -- String  Literal
-                        |   TmLam       SourceTyp SourceTm      -- Abstraction with binding
-                        |   TmClos      SourceTm SourceTyp SourceTm
+                        |   TmLam       Params SourceTm      -- Abstraction with binding
                         |   TmRec       String SourceTm
                         |   TmRProj     SourceTm String
                         |   TmProj      SourceTm Integer        -- Projection on Expression
                         |   TmApp       SourceTm SourceTm
                         |   TmMrg       SourceTm SourceTm
                         |   TmBox       SourceTm SourceTm
-                        -- Extension that require elaboration
-                        |   TmIf        SourceTm SourceTm SourceTm
-                        |   TmFix       SourceTyp SourceTm
-                        |   TmNil       SourceTyp
-                        |   TmCons      SourceTm SourceTm
-                        -- [SourceTm] should become merges
-                        |   TmTag       SourceTm SourceTyp
-                        |   TmCase      SourceTm [(Pattern, SourceTm)]
+                        -- To be elaborated
+                        |   TmSeq       SourceTm SourceTm
+                        |   TmFunc      Params SourceTm
+                        |   TmTuple     [SourceTm]
+                        |   TmStruct    Params SourceTm
+                        |   TmModule    Params SourceTm
+                        -- Basic operation support
                         |   TmBinOp     BinaryOp SourceTm SourceTm
                         |   TmUnOp      UnaryOp SourceTm
-                        -- Not sure if tagging is needed at source level -- can be simply added during elaboration to core
-                        |   TmAnno      SourceTm SourceTyp
-                        |   TmTuple     [SourceTm]
-                        |   TmStruct    SourceTyp SourceTm
-                        |   TmModule    String SourceTyp SourceTm
                         deriving (Eq, Show)
 
 data SourceTyp          =   TySUnit                             -- Unit type for empty environment
@@ -136,14 +55,12 @@ data SourceTyp          =   TySUnit                             -- Unit type for
                         |   TySRecord   String SourceTyp        -- Single-Field Record Type
                         |   TySBool                             -- Boolean type
                         |   TySString                           -- String type
-                        |   TySList     SourceTyp               -- Type for built-in list 
-                        |   TySUnion    SourceTyp SourceTyp     -- Type for sums
-                        |   TySPair     SourceTyp SourceTyp     -- Type for pairs
                         |   TySSig      SourceTyp SourceTyp     -- Sig Type End
-                        |   TySIden     String                  -- Simply an alias
                         deriving (Eq, Show)
 
 data CoreTm             =   Ctx                                 -- Context
+                        |   EBool   Bool                        -- Boolean Term
+                        |   EString String                      -- String Term
                         |   Unit                                -- Unit
                         |   Lit    Integer                      -- Integer literal
                         |   Lam    CoreTyp CoreTm               -- Lambda Abstraction
@@ -154,46 +71,26 @@ data CoreTm             =   Ctx                                 -- Context
                         |   App    CoreTm CoreTm                -- Application
                         |   Mrg    CoreTm CoreTm                -- Merge
                         |   Box    CoreTm CoreTm                -- Box
-                        -- Extensions
-                        |   EBool   Bool                        -- Boolean Term
-                        |   EString String                      -- String Term
-                        |   If     CoreTm CoreTm CoreTm         -- Conditionals
-                        |   Fix    CoreTyp CoreTm               -- Recursion
-                        |   Tag    CoreTm CoreTyp
-                        |   Case   CoreTm [(Pattern, CoreTm)]
-                        |   Nil    CoreTyp                      -- Nil List with Type
-                        |   Cons   CoreTm CoreTm                -- List expression
-                        |   LCase  CoreTm CoreTm CoreTm
                         |   BinOp  BinaryOp CoreTm CoreTm       -- Binary operations
                         |   UnOp   UnaryOp CoreTm               -- Unary operations
-                        -- Lambda for linking
-                        |   CLam   CoreTyp  CoreTm
-                        |   Anno   CoreTm   CoreTyp 
                         deriving (Eq, Show, Generic)
 
 data CoreTyp            =   TyCUnit                       -- Unit type for empty environment
+                        |   TyCBool                       -- Boolean type
+                        |   TyCString                     -- String type
                         |   TyCInt                        -- Integer type
                         |   TyCAnd        CoreTyp CoreTyp -- Intersection type
                         |   TyCArrow      CoreTyp CoreTyp -- Arrow type, e.g. A -> B
                         |   TyCRecord     String  CoreTyp -- Single-Field Record Type
-                        -- Extensions
-                        |   TyCUnion      CoreTyp CoreTyp -- Union type
-                        |   TyCList       CoreTyp         -- List Type
-                        |   TyCBool                       -- Boolean type
-                        |   TyCString                     -- String type
                         deriving (Eq, Show, Generic)
 
 data Value              =       VUnit                      -- Unit value
                         |       VInt    Integer            -- Integer value
+                        |       VBool   Bool               -- Boolean Value
+                        |       VString String             -- String Value
                         |       VClos   Value CoreTm       -- Closure
                         |       VRcd    String Value       -- Single-field record value
                         |       VMrg    Value Value        -- Merge of two values
-                        -- Extensions
-                        |       VNil    CoreTyp
-                        |       VCons   Value Value
-                        |       VTag    Value CoreTyp      
-                        |       VBool   Bool               -- Boolean Value
-                        |       VString String             -- String Value
                         deriving (Show, Eq, Generic)
 
 instance Binary CoreTm
